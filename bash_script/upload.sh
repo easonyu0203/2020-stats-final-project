dcf478611dd334a08a6d3da0ac829d4788cdd003
eason <breaking0203@gmail.com>
2020-12-04 05:46:12 +0800

    upload data

--- bash_script/upload.sh
+++ bash_script/upload.sh
@@ -1,6 +1,8 @@
 # write data to excel and to github
 dir="/Users/eason/Desktop/statistics/final-project"
 echo "**************************************************"
+now=$(date +"%x %X")
+echo "current time: ${now}"
 echo "writing data to excel..."
 /opt/anaconda3/bin/python ${dir}/toExcel.py
 echo "uploading data to github..."
